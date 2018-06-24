524395097115e43c34cb9861302658870ef42c28
Conan <Conan@YuTingde-Mac-mini.local>
2018-06-24 13:23:20 +0800

    20180624 nginx try x-sendfile

--- nginx_php/files/php/ask.php
+++ nginx_php/files/php/ask.php
@@ -3,25 +3,29 @@
     date_default_timezone_set("UTC"); 
 
 	session_start();
-    $sessionID =session_id();
-    
+    $sessionID=session_id();
+    error_log('ask sess'.$sessionID);
     $red_ser=$_SERVER['REDIS_SERVER'];
     $red_port=$_SERVER['REDIS_PORT'];
 
     define("FILE_QUERY_PATH", "/home/ubuntu/www/php/sess/");
     $file_name = "ask.txt";
     $query_folder = FILE_QUERY_PATH.$sessionID."/";
+   
     if (!file_exists($query_folder))//check query session folder exist
     {
         mkdir($query_folder, 0777, true);
     }
+    $timestr = sprintf("%d",time());
+    $ticket = hash('ripemd256',$timestr.$sessionID.'mo.png');
+    
     $file_path = FILE_QUERY_PATH.$sessionID."/".$file_name;
     $file = fopen($file_path, "w");
-    fwrite($file, 'Hello Hello'.PHP_EOL.'You are goodgood');
+    fwrite($file, 'http://localhost:55555/seek.php?ticket='.$ticket.'&timestmp='.$timestr);
     fclose($file);
     // $date = new DateTime('now');
     // $date->format('c');
-    $rkey = hash('gost',sprintf("%d",time()).$sessionID);
+    $rkey = hash('gost',$timestr.$sessionID);
     $a = array('path' => $file_path, 'expired' => 180);
     list($ret,$redis)=RedisConnect($red_ser, $red_port);
     if($ret==0){
--- nginx_php/files/php/seek.php
+++ nginx_php/files/php/seek.php
@@ -1,4 +1,25 @@
-#!/usr/bin/php
 <?php
-
+    session_start();
+    $sessionID=session_id();
+    error_log('seek sess'.$sessionID);
+    if (isset($_GET['ticket'])&&isset($_GET['timestamp']))
+    {
+        $tic = $_GET["ticket"];
+        $timestamp = $_GET["timestamp"];
+        if($tic==hash('ripemd256',$timestamp.$sessionID.'mo.png')){
+            $aliasedFile = '/download/mo.png'; //this is the nginx alias of the file path
+            $realFile = '/home/ubuntu/www/php/protected/mo.png'; //this is the physical file path
+            $filename = 'download.png'; //this is the file name user will get
+            header('Cache-Control: public, must-revalidate');
+            header('Pragma: no-cache');
+            header('Content-Type: application\pdf');
+            header('Content-Length: ' .(string)(filesize($realFile)) );
+            header('Content-Disposition: attachment; filename='.$filename.'');
+            header('Content-Transfer-Encoding: binary');
+            header('X-Accel-Redirect: '. $aliasedFile);
+            return;
+        }
+    }
+    http_response_code(404);
+    return;
 ?>
